bf8f0bbfc473225f6929b149b808218f369a35c1
shaik849 <sanavullashaik0@gmail.com>
2025-04-03 12:34:40 +0530

    versel 404 issue comit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
+  plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist' // Ensure build output is in "dist"
+    outDir: "dist" // Ensure the build output is in "dist"
+  },
+  server: {
+    port: 3000
   }
-})
+});
