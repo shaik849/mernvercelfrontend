0e20eb19353811a0c7cd9d3059da090c2733ca3c
shaik849 <sanavullashaik0@gmail.com>
2025-04-03 12:18:11 +0530

    vercel issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
+  build: {
+    outDir: 'dist' // Ensure build output is in "dist"
+  }
 })
